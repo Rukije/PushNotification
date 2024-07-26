c66cc34fb1c4b00115b4018d606c00e335bd16c2
rukije <rukije.morina@harrisia.com>
2024-07-26 15:05:09 +0200

    Local notification

--- App.js
+++ App.js
@@ -1,10 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Button, Alert } from "react-native";
+import * as Notifications from 'expo-notifications';
+import { useEffect } from 'react';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  }
+});
+
+async function requestPermissionsAsync() {
+  const { status } = await Notifications.getPermissionsAsync();
+  if (status !== 'granted') {
+    const { status: newStatus } = await Notifications.requestPermissionsAsync();
+    return newStatus === 'granted';
+  }
+  return status === 'granted';
+}
 
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      const granted = await requestPermissionsAsync();
+      if (!granted) {
+        Alert.alert('Permissions required', 'Push notifications permissions are required for this app.');
+      }
+    })();
+  }, []);
+
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first local notification",
+        body: "This is the body of the notification",
+        data: { userName: 'Morina' }
+      },
+      trigger: {
+        seconds: 2
+      }
+    }).then(() => {
+      console.log('Notification scheduled'); 
+    }).catch(error => {
+      console.error('Error scheduling notification:', error);  
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +63,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
